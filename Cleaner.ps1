d8f5e4d6c58b8b9f6377fb16919adf5a631fc83f
siddartha21 <34330704+siddartha21@users.noreply.github.com>
2021-02-05 09:48:43 +0530

    Add files via upload

---
+++ Cleaner.ps1
@@ -0,0 +1,5 @@
+﻿$logpath = "C:\Users\*\AppData\Local\UiPath\Logs"
+
+#Delete logs older than 3 days
+
+Get-ChildItem $logpath -Recurse -Include *_Terminal.log, *_VisionHost.log, *_Execution.log -force | where {$_.lastwritetime -lt (get-date).adddays(-3)} | Remove-Item -force 
